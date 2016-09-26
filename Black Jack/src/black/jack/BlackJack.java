639c41fa46a71c8417ea5e8499b6a4c9d747c538
oop <oop@pc01>
2016-09-26 07:42:52 -0500

    Black Jack

---
+++ Black Jack/src/black/jack/BlackJack.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package black.jack;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author oop
+ */
+
+
+public class BlackJack extends JFrame{
+
+    private MyButton b[]; 
+    private  JLabel d;      
+    
+    
+    //--------------------------------------------
+    public BlackJack()
+    {
+        
+    }
+    private void widgets()
+    {
+        
+    }
+    
+    private void events()
+    {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }    
+}
---
+++ Black Jack/src/black/jack/MyButton.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package black.jack;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author oop
+ */
+public class MyButton extends JButton {
+    
+    private int v;
+    //----------------------------
+    public MyButton(int v)
+    {
+        this.v = v;
+    }
+    public void show()
+    {
+        setText(""+v);
+    }
+    public int getValue ()
+    {
+        return v;
+    } 
+}
